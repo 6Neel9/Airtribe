d51f26bfd68831a69940ce35abf4b9fc813c7419
6Neel9 <nilmandal300@gmail.com>
2025-09-21 16:57:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function p1(){
+    var a = 20;
+    function c1(){
+        let a = 10;
+        console.log(a);
+        function c2(){
+            console.log(a);
+            a=30;
+            
+        }
+        c2();
+        console.log(a);
+
+    }
+    c1()
+}
+p1()
